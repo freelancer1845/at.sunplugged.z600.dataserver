f30ce922dc1c381fea0ff3fda30ce25d6dc8e2de
freelancer1845 <jaschelite@googlemail.com>
2018-05-21 21:14:38 +0200

    Release Cross Origin

--- backend/src/main/java/at/sunplugged/z600/dataserver/z600dataserver/controller/CrossOriginConfig.java
+++ backend/src/main/java/at/sunplugged/z600/dataserver/z600dataserver/controller/CrossOriginConfig.java
@@ -2,7 +2,7 @@ package at.sunplugged.z600.dataserver.z600dataserver.controller;
 
 public class CrossOriginConfig {
 
-	public static final String ALLOWED_CROSS_ORIGIN = "http://localhost:4200";
+  public static final String ALLOWED_CROSS_ORIGIN = "http://192.168.178.*";
 
-	public static final int MAX_AGE = 3600;
+  public static final int MAX_AGE = 3600;
 }
